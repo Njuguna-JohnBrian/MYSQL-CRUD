77b620b322826f65c72831e5fff87ec39abe0b16
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-11-08 07:15:55 +0300

    Add getUser, updateUser and deleteUser Services

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,7 +1,17 @@
-const { createUser } = require("./user.controller");
+const {
+  createUser,
+  getUserByUserId,
+  getUsers,
+  updateUsers,
+  deleteUser,
+} = require("./user.controller");
 
 const router = require("express").Router();
 
 router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/", getUserByUserId);
+router.patch("/", updateUsers);
+router.delete("/", deleteUser)
 
 module.exports = router;

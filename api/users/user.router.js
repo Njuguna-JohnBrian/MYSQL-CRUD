a24876ae373c164767aee7687e6533dcd86a669b
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-11-08 08:54:20 +0300

    Update

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -6,7 +6,7 @@ const {
   deleteUser,
 } = require("./user.controller");
 
-const router = require("express").Router();
+const router = require("express").Router(); 
 
 router.post("/", createUser);
 router.get("/", getUsers);
